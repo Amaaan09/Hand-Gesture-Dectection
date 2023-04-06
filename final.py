dd4f5e1ebaaa5194ce66078367831517ab1c9e19
Amaan Ansari <95982010+Amaaan09@users.noreply.github.com>
2023-04-06 15:17:09 +0530

    uh

--- final.py
+++ final.py
@@ -2,7 +2,11 @@ import cv2
 import streamlit as st
 
 # Initialize OpenCV webcam capture
-cap = cv2.VideoCapture(0)
+try:
+    cap = cv2.VideoCapture(0)
+except Exception as e:
+    st.error(f"Error: {e}")
+    st.stop()
 
 def main():
     st.title("Webcam Feed")
@@ -11,9 +15,14 @@ def main():
         # Read frame from webcam
         ret, frame = cap.read()
         if not ret:
+            st.error("Error reading from webcam")
             break
             
         # Flip the frame horizontally for a selfie-view display
         frame = cv2.flip(frame, 1)
         
-        # Display
+        # Display the frame in Streamlit
+        st.image(frame, channels="BGR")
+
+if __name__ == '__main__':
+    main()
